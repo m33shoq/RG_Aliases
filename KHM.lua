9d75fd256fc91bbfe17493f1f7a401b8207ea5c7
m33shoq <mishadyrov@gmail.com>
2023-10-27 14:09:52 +0300

    10.2 compability, version dump to 3

--- KHM.lua
+++ KHM.lua
@@ -1,4 +1,5 @@
 local addonName, addonTable = ...
+local IsAddOnLoaded = IsAddOnLoaded or C_AddOns.IsAddOnLoaded
 
 function addonTable.HookKHM()
 	if IsAddOnLoaded("KHMRaidFrames") then
@@ -7,36 +8,36 @@ function addonTable.HookKHM()
 		print("|cffee5555[Rak Gaming Aliases]|r KHM HOOKED")
 		function KHMRaidFrames.SetUpNameInternal(frame, groupType)
 			if not frame.name then return end
-		
+
 			local db = KHMRaidFrames.db.profile[groupType].nameAndIcons.name
-		
+
 			if not db.enabled then
 				return
 			end
-		
+
 			if not frame.unit then return end
 			if not UnitExists(frame.displayedUnit) then return end
 			if not ShouldShowName(frame) then return end
-		
+
 			local name = frame.name
-		
+
 			local _name
-		
+
 			if db.showServer then-- change here to catch either RG_UnitName or GetUnitName
 				_name = RG_UnitName(frame.unit) or GetUnitName(frame.unit, true)
 			else
 				_name = RG_UnitName(frame.unit) or GetUnitName(frame.unit, false)
 				-- _name = _name and _name:gsub("%p", "") or _name
 			end
-			
+
 			local oldName = name:GetText()
 			if _name ~= nil and _name:len() > 0 and _name ~= "" and (oldName ~= _name) then
 				name:SetText(_name)
 			end
-		
+
 			if db.classColoredNames then
 				local classColor = KHMRaidFrames.ColorByClass(frame.unit)
-		
+
 				if classColor then
 					name:SetTextColor(classColor.r, classColor.g, classColor.b)
 				end
