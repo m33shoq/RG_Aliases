6e028cfe2698bc9f59cb25b212008758354ba547
m33shoq <mishadyrov@gmail.com>
2025-02-24 17:45:30 +0200

    fix for Cell 244

--- Modules/Cell.lua
+++ Modules/Cell.lua
@@ -17,9 +17,16 @@ function AliasesNamespace.HookCell()
 			local Cell = _G.Cell
 			local F = Cell.funcs
 			local RG_UnitName = AliasesNamespace.RG_UnitName
-			F.GetNickname = function(self,shortName,fullName)
-				return fullName and RG_UnitName(fullName) or shortName and RG_UnitName(shortName) or shortName or _G.UNKNOWNOBJECT
+			if Cell.versionNum >= 244 then -- in 244 Cell changed methods from colon to dot
+				F.GetNickname = function(shortName,fullName)
+					return fullName and RG_UnitName(fullName) or shortName and RG_UnitName(shortName) or shortName or _G.UNKNOWNOBJECT
+				end
+			else
+				F.GetNickname = function(self,shortName,fullName)
+					return fullName and RG_UnitName(fullName) or shortName and RG_UnitName(shortName) or shortName or _G.UNKNOWNOBJECT
+				end
 			end
+
 		end)
 	end
 end
