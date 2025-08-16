151dbe7b1cd862a95c47c0678c2c4cd98c465222
m33shoq <mishadyrov@gmail.com>
2025-08-16 17:18:01 +0300

    remove Cell pre-244 compat code

--- Modules/Cell.lua
+++ Modules/Cell.lua
@@ -17,16 +17,10 @@ function AliasesNamespace.HookCell()
 			local Cell = _G.Cell
 			local F = Cell.funcs
 			local RG_UnitName = AliasesNamespace.RG_UnitName
-			if Cell.versionNum >= 244 then -- in 244 Cell changed methods from colon to dot
-				F.GetNickname = function(shortName,fullName)
-					return fullName and RG_UnitName(fullName) or shortName and RG_UnitName(shortName) or shortName or _G.UNKNOWNOBJECT
-				end
-			else
-				F.GetNickname = function(self,shortName,fullName)
-					return fullName and RG_UnitName(fullName) or shortName and RG_UnitName(shortName) or shortName or _G.UNKNOWNOBJECT
-				end
-			end
 
+			F.GetNickname = function(shortName,fullName)
+				return fullName and RG_UnitName(fullName) or shortName and RG_UnitName(shortName) or shortName or _G.UNKNOWNOBJECT
+			end
 		end)
 	end
 end
